a0bc41dd258be086011a4fc52dd5d10257234945
lukesubuntu <luke@hardiman.co.nz>
2020-03-20 09:15:56 +1300

    added php version and online example

---
+++ PHP/index.php
@@ -0,0 +1,25 @@
+<?php
+$response_data = file_get_contents("https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases");
+
+$dom = new DOMDocument;
+$dom->loadHTML($response_data);
+$dom->validateOnParse = true;
+
+$covidResults = [];
+$covidResponse = $dom->getElementsByTagName('tbody')->item(0)->getElementsByTagName('tr');
+
+foreach ($covidResponse as $index => $result) {
+  
+  $results = explode("\n", str_replace("\t", "" , $result->nodeValue));
+  $covidResults[] = array(
+    "case" => $results[1],
+    "location" => $results[2],
+    "age" => $results[3],
+    "gender" => $results[4],
+    "notes" => $results[5]
+  );
+ 
+}
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+echo json_encode($covidResults);
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # covid-19-nzdata
-scraps/extracts new zealand covid-19 cases from https://www.health.govt.nz/ into json format
-
+PHP & NODE scripts that scraps/extracts New Zealand covid-19 cases from https://www.health.govt.nz/ into json format.  
+Other data sources seem to be slow on updating NZ cases.  
+      
+Online data example [https://cdn.nz/covid-19-nzdata](https://cdn.nz/covid-19-nzdata). 
+      
+    
 ![Screen Shot](http://cdn.nz/screenshot.png)
