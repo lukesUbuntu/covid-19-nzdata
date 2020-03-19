2a12a9082eee7dc4b7c8e375a786f86079e2bb7f
lukesubuntu <luke@hardiman.co.nz>
2020-03-19 18:39:28 +1300

    added server and format

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 "use strict"
-
+const http = require('http');
 const Xray = require('x-ray')
-const req = Xray()
+
+const PORT = "8080";
 const dataURL = "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases";
-const NZCOVID = [];
-req(dataURL, 'table tbody', [{
-    results: req('tr', ['tr'])
-}]).then(function(response) {
 
-    const table = response[0]['results'];
-    table.forEach(result => {
-        NZCOVID.push(result.replace(/\t/g, "").split("\n"))
-    });
 
-    console.log("element", NZCOVID)
+var scrapeData = new Promise(function(resolve, reject) {
+    const req = Xray()
+    const resultSet = [];
+    req(dataURL, 'table tbody', [{
+        results: req('tr', ['tr'])
+    }]).then(function(response) {
+
+        const table = response[0]['results'];
+        table.forEach(result => {
+            result = result.replace(/\t/g, "").split("\n");
+            resultSet.push({
+                number: result[1],
+                location: result[2],
+                age: result[3],
+                gender: result[4],
+                notes: result[5]
+            });
+
+        });
+
+        resolve(resultSet)
+    })
 })
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    });
+    scrapeData.then(function(responseData) {
+        response.write(JSON.stringify(responseData));
+        response.end();
+    })
+
+}).listen(PORT);
+
+console.log("Listening on port " + PORT);
