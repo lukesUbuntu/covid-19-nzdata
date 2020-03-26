eda268fd37c069cc91b5ae5a9f2346d82d76a0e1
lukesubuntu <luke@hardiman.co.nz>
2020-03-26 15:04:52 +1300

    updated from table changes

--- PHP/index.php
+++ PHP/index.php
@@ -2,23 +2,22 @@
 $response_data = file_get_contents("https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases?".rand());
 
 $dom = new DOMDocument;
-$dom->loadHTML($response_data);
-$dom->validateOnParse = true;
+@$dom->loadHTML($response_data);
+// $dom->validateOnParse = true;
 
 $covidResults = [];
-$covidResponse = $dom->getElementsByTagName('tbody')->item(0)->getElementsByTagName('tr');
+$covidResponse = $dom->getElementsByTagName('tbody')->item(1)->getElementsByTagName('tr');
 
 foreach ($covidResponse as $index => $result) {
-  
-  $results = explode("\n", str_replace("\t", "" , $result->nodeValue));
-  $covidResults[] = array(
-    "case" => $results[1],
-    "location" => $results[2],
-    "age" => $results[3],
-    "gender" => $results[4],
-    "notes" => $results[5]
-  );
  
+  $results = explode("\n", str_replace("\t", "" , $result->nodeValue));
+  if ($results[1] === "DHB" || $results[1] === "Total") continue;
+  $covidResults[] = array(
+    "dhb" => $results[1],
+    "confirmed" => $results[2],
+    "probable" => $results[3],
+    "total" => $results[4],
+  );
 }
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
